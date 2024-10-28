6cb90a098773c20f0586cdb9f3a5181a3ebb59f9
Steve Young <syoung@derryfield.org>
2024-10-28 10:25:32 -0400

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,2 @@
+rootProject.name = "DSp2Stack"
+
---
+++ src/main/java/StackInterface.java
@@ -0,0 +1,23 @@
+/**
+ * An interface for a last-in-first-out (LIFO) stack structure,
+ * similar to the <code>java.util.Stack</code> class.
+ * <p></p>
+ * The data in the stack will be stored in <code>Node</code> objects,
+ * with each node storing a data element of type <code>T</code>,
+ * and a reference to the next node. For this project, a <code>Node</code>
+ * class has been provided, but you can choose to modify this project structure.
+ * <p></p>
+ * The goal of this project is to implement this interface, so you may not
+ * use Java library stack implementations, such as <code>java.util.Vector</code>,
+ * <code>java.util.Deque</code> implementations, or the Java Collections Framework.
+ * <p></p>
+ * This short project is similar to the linked list implementation that you built
+ * for the previous project, with <code>push</code> replacing <code>addFirst</code>,
+ * and <code>pop</code> replacing <code>delete</code> with index 0.
+ * You will be expected to check for null pointer and out of bounds exceptions.
+ * @author jcollard, jddevaug
+ * @param <T> the type of elements in the stack
+ */
+public interface StackInterface<T> {
+
+}
